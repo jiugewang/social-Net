398509c0a6351a94e31fab2cac347d14d1bb67de
jiuge <799605487@qq.com>
2018-08-19 20:12:57 +0800

    jiuge
    安装tensorflow

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import numpy as np
 import networkx as nx
+import tensorflow as tf
 matrix = np.array([3, 4])
 print(matrix)
 
 G = nx.Graph()
 G.add_edge(1, 2)
 print(G.edges())
+
+a = tf.Variable(3)
+print(a)
+
+
